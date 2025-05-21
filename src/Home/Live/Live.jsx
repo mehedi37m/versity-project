4c2dd1a27596b18fe551fc252bb5ce0c64d367df
mehedi37m <mz.mehedi37@gmail.com>
2025-05-22 01:09:30 +0600

    add coursor color

--- src/Home/Live/Live.jsx
+++ src/Home/Live/Live.jsx
@@ -1,4 +1,7 @@
 import Ballpit from "../../../Reactbits/Ballpit/Ballpit";
+import SplashCursor from "../../../Reactbits/SplashCursor/SplashCursor";
+
+
 
 
 
@@ -7,12 +10,12 @@ const Live = () => {
     return (
         <div>
           
+          <SplashCursor />
 
-<div className="text-5xl">hello</div>
 
-<div style={{position: 'relative', overflow: 'hidden', minHeight: '500px', maxHeight: '500px', width: '100%'}}>
-  <Ballpit  count={200}
-    gravity={0.7}
+<div style={{position: 'relative', overflow: 'hidden', minHeight: '500px', maxHeight: '500px', width: '100%', backgroundColor:"white"}}>
+  <Ballpit  count={50}
+    gravity={2}
     friction={0.8}
     wallBounce={0.95}
     followCursor={true}></Ballpit>
